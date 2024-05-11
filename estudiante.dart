173e32008404e035d8d379897d797d4489a73ec1
Juano1927 <j.david1927@hotmail.com>
2024-05-11 14:45:19 -0500

    final

--- estudiante.dart
+++ estudiante.dart
@@ -1,31 +1,35 @@
+import 'dart:io';
+
 class Estudiante {
   String cedula;
   String nombre;
   String apellido;
   String fechaNac;
   List<double> notas = [];
-  double promedio;
+  double promedio = 0.0;
   String direccion;
   String telefono;
 
   Estudiante(this.cedula, this.nombre, this.apellido, this.fechaNac,
       this.direccion, this.telefono);
-
   void cambiarDireccion(String nuevaDireccion) {
     direccion = nuevaDireccion;
   }
 
   void mostrarUsuario() {
-    print('Cédula: $cedula');
-    print('Nombre: $nombre $apellido');
+    print('Cedula: $cedula');
+    print('Nombre: $nombre');
+    print('Apellido: $apellido');
     print('Fecha de Nacimiento: $fechaNac');
     print('Dirección: $direccion');
     print('Teléfono: $telefono');
     print('Notas: $notas');
     print('Promedio: $promedio');
   }
-}
 
-bool validarCedula(String cedula) {
-  return cedula.isNotEmpty && cedula.contains(RegExp(r'^[0-9]+$'));
+  void calcularPromedio() {
+    if (notas.isNotEmpty) {
+      promedio = notas.reduce((a, b) => a + b) / notas.length;
+    }
+  }
 }
--- main.dart
+++ main.dart
@@ -6,20 +6,25 @@ void main() {
 
   while (true) {
     print('\nMenú:');
-    print('1. Registrar Estudiante');
-    print('2. Ver/Modificar Información de Estudiante');
-    print('3. Salir');
-    stdout.write('Ingrese su opción: ');
-    String opcion = stdin.readLineSync()!;
+    print('1. Registrarse estudiante');
+    print('2. Ver información de estudiante');
+    print('3. Modificar dirección de estudiante');
+    print('4. Salir');
+    print('Seleccione una opción:');
+
+    var opcion = int.parse(stdin.readLineSync()!);
 
     switch (opcion) {
-      case '1':
+      case 1:
         registrarEstudiante(estudiantes);
         break;
-      case '2':
-        verModificarEstudiante(estudiantes);
+      case 2:
+        verInformacionEstudiante(estudiantes);
         break;
-      case '3':
+      case 3:
+        modificarDireccion(estudiantes);
+        break;
+      case 4:
         print('Saliendo del programa...');
         return;
       default:
@@ -29,126 +34,90 @@ void main() {
 }
 
 void registrarEstudiante(List<Estudiante> estudiantes) {
-  stdout.write('\nIngrese la cédula del estudiante: ');
-  String cedula = stdin.readLineSync()!;
+  print('\nIngrese la cedula del estudiante:');
+  var cedula = stdin.readLineSync()!;
 
-  // Validar cédula
-  if (!validarCedula(cedula)) {
-    print('La cédula debe contener solo números. Intente de nuevo.');
-    return;
-  }
+  print('Ingrese el nombre del estudiante:');
+  var nombre = stdin.readLineSync()!;
 
-  stdout.write('Ingrese el nombre del estudiante: ');
-  String nombre = stdin.readLineSync()!;
-  stdout.write('Ingrese el apellido del estudiante: ');
-  String apellido = stdin.readLineSync()!;
-  stdout.write('Ingrese la fecha de nacimiento (AAAA-MM-DD): ');
-  String fechaNac = stdin.readLineSync()!;
+  print('Ingrese el apellido del estudiante:');
+  var apellido = stdin.readLineSync()!;
 
-  // Validar formato de fecha de nacimiento
-  if (!validarFormatoFecha(fechaNac)) {
-    print(
-        'El formato de fecha de nacimiento es incorrecto. Debe ser AAAA-MM-DD. Intente de nuevo.');
-    return;
-  }
+  print('Ingrese la fecha de nacimiento (AAAA-MM-DD):');
+  var fechaNac = stdin.readLineSync()!;
 
-  stdout.write(
-      'Ingrese la dirección del estudiante (Formato: Calle, Ciudad, País): ');
-  String direccion = stdin.readLineSync()!;
+  print('Ingrese la dirección del estudiante:');
+  var direccion = stdin.readLineSync()!;
 
-  // Validar formato de dirección
-  if (!validarFormatoDireccion(direccion)) {
-    print(
-        'El formato de dirección es incorrecto. Debe ser Calle, Ciudad, País. Intente de nuevo.');
-    return;
-  }
+  print('Ingrese el teléfono del estudiante:');
+  var telefono = stdin.readLineSync()!;
 
-  stdout.write('Ingrese el teléfono del estudiante: ');
-  String telefono = stdin.readLineSync()!;
-
-  Estudiante nuevoEstudiante =
+  var nuevoEstudiante =
       Estudiante(cedula, nombre, apellido, fechaNac, direccion, telefono);
+  nuevoEstudiante.notas = pedirNotas();
 
-  // Ingresar notas
-  for (int i = 0; i < 5; i++) {
-    stdout.write('Ingrese la nota ${i + 1}: ');
-    double nota = double.tryParse(stdin.readLineSync()) ?? -1;
-    // Validar rango de notas
-    if (nota < 0 || nota > 5) {
-      print('La nota debe estar entre 0 y 5. Intente de nuevo.');
-      return;
-    }
-    nuevoEstudiante.notas.add(nota);
-  }
-
-  // Calcular promedio
-  double promedio = nuevoEstudiante.notas.reduce((a, b) => a + b) /
-      nuevoEstudiante.notas.length;
-  nuevoEstudiante.promedio = promedio;
-
-  // Agregar estudiante a la lista
+  nuevoEstudiante.calcularPromedio();
   estudiantes.add(nuevoEstudiante);
 
-  print('Estudiante registrado exitosamente.');
+  print('Estudiante registrado correctamente.');
 }
 
-void verModificarEstudiante(List<Estudiante> estudiantes) {
+List<double> pedirNotas() {
+  List<double> notas = [];
+  for (var i = 0; i < 5; i++) {
+    print('Ingrese la nota ${i + 1}:');
+    var nota = double.parse(stdin.readLineSync()!);
+    if (nota < 0 || nota > 5) {
+      print('La nota debe estar entre 0 y 5. Inténtelo de nuevo.');
+      i--;
+      continue;
+    }
+    notas.add(nota);
+  }
+  return notas;
+}
+
+void verInformacionEstudiante(List<Estudiante> estudiantes) {
   if (estudiantes.isEmpty) {
     print('No hay estudiantes registrados.');
     return;
   }
 
-  stdout.write('\nIngrese la cédula del estudiante que desea buscar: ');
-  String cedula = stdin.readLineSync()!;
-  Estudiante? estudianteEncontrado;
-
-  for (var estudiante in estudiantes) {
-    if (estudiante.cedula == cedula) {
-      estudianteEncontrado = estudiante;
-      break;
-    }
+  print('\nLista de estudiantes:');
+  for (var i = 0; i < estudiantes.length; i++) {
+    print('$i. ${estudiantes[i].nombre} ${estudiantes[i].apellido}');
   }
 
-  if (estudianteEncontrado == null) {
-    print('Estudiante no encontrado.');
+  print('Seleccione el número de estudiante para ver su información:');
+  var index = int.parse(stdin.readLineSync()!);
+  if (index < 0 || index >= estudiantes.length) {
+    print('Número de estudiante no válido.');
     return;
   }
 
-  estudianteEncontrado.mostrarUsuario();
+  estudiantes[index].mostrarUsuario();
+}
 
-  print('\n¿Desea modificar alguna nota? (s/n)');
-  String opcionModificarNota = stdin.readLineSync()!;
-  if (opcionModificarNota.toLowerCase() == 's') {
-    // Modificar nota
-    for (int i = 0; i < estudianteEncontrado.notas.length; i++) {
-      stdout.write('Ingrese la nueva nota ${i + 1}: ');
-      double nuevaNota = double.tryParse(stdin.readLineSync()) ?? -1;
-      // Validar rango de notas
-      if (nuevaNota < 0 || nuevaNota > 5) {
-        print('La nota debe estar entre 0 y 5. Intente de nuevo.');
-        return;
-      }
-      estudianteEncontrado.notas[i] = nuevaNota;
-    }
-    // Recalcular promedio
-    double nuevoPromedio = estudianteEncontrado.notas.reduce((a, b) => a + b) /
-        estudianteEncontrado.notas.length;
-    estudianteEncontrado.promedio = nuevoPromedio;
-    print('Notas modificadas exitosamente. Promedio recalculado.');
+void modificarDireccion(List<Estudiante> estudiantes) {
+  if (estudiantes.isEmpty) {
+    print('No hay estudiantes registrados.');
+    return;
   }
 
-  // Aquí podrías añadir más opciones para modificar otros datos del estudiante, como la dirección.
-}
+  print('\nLista de estudiantes:');
+  for (var i = 0; i < estudiantes.length; i++) {
+    print('$i. ${estudiantes[i].nombre} ${estudiantes[i].apellido}');
+  }
 
-bool validarCedula(String cedula) {
-  return cedula.isNotEmpty && cedula.contains(RegExp(r'^[0-9]+$'));
-}
+  print('Seleccione el número de estudiante para modificar su dirección:');
+  var index = int.parse(stdin.readLineSync()!);
+  if (index < 0 || index >= estudiantes.length) {
+    print('Número de estudiante no válido.');
+    return;
+  }
 
-bool validarFormatoFecha(String fecha) {
-  return RegExp(r'^\d{4}-\d{2}-\d{2}$').hasMatch(fecha);
-}
-
-bool validarFormatoDireccion(String direccion) {
-  return RegExp(r'^[a-zA-Z0-9\s\-,]+,[a-zA-Z\s\-,]+,[a-zA-Z\s\-,]+$')
-      .hasMatch(direccion);
+  print('Ingrese la nueva dirección:');
+  var nuevaDireccion = stdin.readLineSync()!;
+  estudiantes[index].cambiarDireccion(nuevaDireccion);
+  print('Dirección modificada correctamente.');
 }
